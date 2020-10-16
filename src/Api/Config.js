7174a352aaba270687c00defbbfe2bb10eb7d0bc
trakbit <harsh.vardhan.611@gmail.com>
2020-10-16 23:38:49 +0530

    Update Config.js

--- src/Api/Config.js
+++ src/Api/Config.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-const STAG_URL_HEROKU = 'https://orderbump.herokuapp.com/';
 const LOCALHOST = 'http://127.0.0.1:8000/';
-const RENDER_URL = 'https://salesjump.onrender.com/';
+const RENDER_URL = 'https://squarespace.onrender.com';
 
 const defaultAxiosOptions = {
-    baseURL: LOCALHOST
+    baseURL: RENDER_URL
 };
 
 export default fetch = axios.create(defaultAxiosOptions);
